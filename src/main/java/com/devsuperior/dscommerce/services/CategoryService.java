32bce599bcfb600477b0d34ac633aea6958c9364
Renato Teixeira <renatous93@gmail.com>
2025-02-21 18:12:00 +0000

    GET categories

---
+++ src/main/java/com/devsuperior/dscommerce/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<CategoryDTO> findAll() {
+		List<Category> result = repository.findAll();
+		return result.stream().map(x -> new CategoryDTO(x)).toList();
+	}
+}
