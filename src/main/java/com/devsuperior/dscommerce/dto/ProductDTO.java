799ea761ebd5adca2ea5b3706fa7050e2912fba0
Renato Teixeira <renatous93@gmail.com>
2025-02-21 18:18:00 +0000

    Fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.devsuperior.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -20,6 +21,7 @@ public class ProductDTO {
 	@NotBlank(message = "Campo requerido")
 	@Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
 	private String description;
+	@NotNull(message = "Campo requerido")
 	@Positive(message = "O preço deve ser positivo")
 	private Double price;
 	private String imgUrl;
